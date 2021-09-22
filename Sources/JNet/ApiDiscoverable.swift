9a4b5cda00decce5351bc779d78e2411588c9d4d
Jarrod Parkes <parkesfjarrod@gmail.com>
2021-09-22 08:12:13 -0500

    deduce api jsonable method to use urlrequest at core

--- Sources/JNet/ApiDiscoverable.swift
+++ Sources/JNet/ApiDiscoverable.swift
@@ -14,7 +14,25 @@ public protocol ApiDiscoverable: AnyObject {
 // MARK: - APIJsonable: ApiDiscoverable
 
 public protocol APIJsonable: ApiDiscoverable {
+    func request<T: Codable>(_ request: URLRequest,
+                             success: ((T) -> Void)?,
+                             failure: RequestFailure) -> FetchOperation?
+
     func request<T: Codable>(_ request: Request,
                              success: ((T) -> Void)?,
                              failure: RequestFailure) -> FetchOperation?
+}
+
+// MARK: - APIJsonable (Defaults)
+
+extension APIJsonable {
+    func request<T: Codable>(_ request: Request,
+                             success: ((T) -> Void)?,
+                             failure: RequestFailure) -> FetchOperation? {
+        if let urlRequest = request.urlRequest(forAPI: self) {
+            return self.request(urlRequest, success: success, failure: failure)
+        } else {
+            return nil
+        }
+    }
 }
