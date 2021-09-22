cb2e53616414e157d50794f0525f53a705190228
Jarrod Parkes <parkesfjarrod@gmail.com>
2021-09-22 09:32:01 -0500

    update access for fetcher

--- Sources/JNet/Fetcher.swift
+++ Sources/JNet/Fetcher.swift
@@ -2,7 +2,7 @@ import Foundation
 
 // MARK: - Fetcher
 
-class Fetcher<WrapperType: Codable, RecordType: Codable> {
+class Fetcher<ResponseType: Codable, RecordType: Codable> {
 
     // MARK: Properties
 
@@ -15,12 +15,15 @@ class Fetcher<WrapperType: Codable, RecordType: Codable> {
     private var nextQueryItems: [String: String?] = [:]
 
     private var _records: [RecordType] = []
-    var records: [RecordType] { return _records }
+    /// Ordered array of objects captured during fetches.
+    public var records: [RecordType] { return _records }
 
     private var _fetchedAllRecords = false
-    var fetchedAllRecords: Bool { return _fetchedAllRecords }
+    /// Have all possible records been fetched?
+    public var fetchedAllRecords: Bool { return _fetchedAllRecords }
 
-    var isFetching: Bool {
+    /// Is the fetcher currently fetching objects?
+    public var isFetching: Bool {
         if let currentRequest = currentRequest {
             return currentRequest.state == .executing
         } else {
@@ -30,19 +33,25 @@ class Fetcher<WrapperType: Codable, RecordType: Codable> {
 
     // MARK: Initializer
 
-    init(api: APIJsonable, coreRequest: Request) {
+    /// Creates a `Fetcher` for a specific `APIJsonable` path represented by `Request`.
+    /// - Parameters:
+    ///   - api: An API identified by its base URL components.
+    ///   - coreRequest: An API request, or endpoint, and its URL components.
+    public init(api: APIJsonable, coreRequest: Request) {
         self.api = api
         self.coreRequest = coreRequest
     }
 
     // MARK: Fetch
 
-    func fetch() {
+    /// Starts a fetch request. If the request is for a paginated endpoint, then each subsequent call
+    /// to this function will request the next page of records.
+    public func fetch() {
         guard let nextRequest = nextRequest(), !_fetchedAllRecords else { return }
 
         if isFetching { cancelRequest() }
 
-        let success = { [weak self] (response: WrapperType) in
+        let success = { [weak self] (response: ResponseType) in
             guard let self = self else { return }
 
             // update next request values
@@ -53,7 +62,7 @@ class Fetcher<WrapperType: Codable, RecordType: Codable> {
             let nextValuesEmpty = self.nextQueryItems.isEmpty && self.nextQueryItems.isEmpty
 
             // convert data to records
-            let newRecords = self.dataToRecords(response: response)
+            let newRecords = self.responseToRecords(response: response)
 
             // no more records or next records, then everything has been fetched...
             if newRecords.isEmpty || nextValuesEmpty {
@@ -123,7 +132,9 @@ class Fetcher<WrapperType: Codable, RecordType: Codable> {
 
     // MARK: Helpers
 
-    func reset() {
+    /// Reset the fetcher such that the next `fetch()` will attempt to fetch a fresh record. If
+    /// the request is for a paginated endpoint, then the next `fetch()` will be for the first page of records.
+    public func reset() {
         cancelRequest()
 
         _fetchedAllRecords = false
@@ -132,29 +143,65 @@ class Fetcher<WrapperType: Codable, RecordType: Codable> {
         nextQueryItems = [:]
     }
 
-    func getRequest() -> Request {
+    /// Return the underlying core request for this fetcher.
+    /// - Returns: The core request for this fetcher.
+    public func getRequest() -> Request {
         return coreRequest
     }
 
-    func setRequest(coreRequest: Request) {
+    /// Update the underlying core request for this fetcher.
+    /// - Parameter coreRequest: New core request.
+    public func setRequest(coreRequest: Request) {
         self.coreRequest = coreRequest
     }
 
     // MARK: Extension Points
 
-    func dataToRecords(response: WrapperType) -> [RecordType] { return [] }
+    /// Extracts an array of records from the top-level response. This is useful is an api endpoint
+    /// returns a top-level JSON object containing an underlying array of objects.
+    /// - Parameter response: The top-level response object.
+    /// - Returns: An array of records to store for the latest fetch request.
+    open func responseToRecords(response: ResponseType) -> [RecordType] { return [] }
 
-    func fetchStarted(firstPage: Bool) {}
-    func fetchedNewRecords(records: [RecordType], firstPage: Bool) {}
-    func fetchFailed(statusCode: Int, error: Error) {}
+    /// A callback when a new fetch starts.
+    /// - Parameter firstPage: Is this fetch for the first page of records?
+    open func fetchStarted(firstPage: Bool) {}
 
-    func nextRequestHeaders(response: WrapperType,
-                            headers: [String: String?]) -> [String: String?] {
+    /// A callback when new records have been fetched.
+    /// - Parameters:
+    ///   - records: An array of newly fetched records.
+    ///   - firstPage: Is this fetch for the first page of records?
+    open func fetchedNewRecords(records: [RecordType], firstPage: Bool) {}
+
+    /// A callback when a fetch fails.
+    /// - Parameters:
+    ///   - statusCode: The HTTP status code for the failure (e.g. 422).
+    ///   - error: The error for the failure.
+    open func fetchFailed(statusCode: Int, error: Error) {}
+
+    /// Returns a dictionary of key/value pairs that will be used to set/override HTTP headers
+    /// for the next fetch.
+    /// - Parameters:
+    ///   - response: The most recent top-level response object.
+    ///   - headers: A dictionary of key/value pairs representing the HTTP headers used
+    ///   for the last fetch.
+    /// - Returns: A dictionary of key/value pairs representing HTTP headers to set/override
+    /// for the next fetch.
+    open func nextRequestHeaders(response: ResponseType,
+                                 headers: [String: String?]) -> [String: String?] {
         return [:]
     }
 
-    func nextRequestQueryItems(response: WrapperType,
-                               items: [String: String?]) -> [String: String?] {
+    /// Returns dictionary of key/value that will be used to set/override query
+    /// items the next fetch.
+    /// - Parameters:
+    ///   - response: The most recent top-level response object.
+    ///   - items: A dictionary of key/value pairs representing the query items used
+    ///   for the last fetch.
+    /// - Returns: A dictionary of key/value pairs representing query items to set/override
+    /// for the next fetch.
+    open func nextRequestQueryItems(response: ResponseType,
+                                    items: [String: String?]) -> [String: String?] {
         return [:]
     }
 }
