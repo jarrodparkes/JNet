b211c36011718a7ae4a81550e61a28d6c8f2d705
Jarrod Parkes <parkesfjarrod@gmail.com>
2022-03-19 15:40:56 -0500

    update platform package deps

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "JNet",
+    platforms: [.iOS(.v12), .macOS(.v10_12), .tvOS(.v12), .watchOS(.v2)],
     products: [.library(name: "JNet", targets: ["JNet"])],
     dependencies: [],
     targets: [
